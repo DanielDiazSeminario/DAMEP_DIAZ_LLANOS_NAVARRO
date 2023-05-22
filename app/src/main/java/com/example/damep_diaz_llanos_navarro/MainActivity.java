d1c2201430817009dd96d20c3d69285ff03c8b9f
Mauricio LLanos <1813011307@untels.edu.pe>
2023-05-22 14:40:12 -0500

    DAMEP LLanos Mauricio Parte2

--- app/src/main/java/com/example/damep_diaz_llanos_navarro/MainActivity.java
+++ app/src/main/java/com/example/damep_diaz_llanos_navarro/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.damep_diaz_llanos_navarro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn1 = findViewById(R.id.button_Enviar);
+        btn1.setOnClickListener(view -> Toast.makeText(this, "Se envio sus datos", Toast.LENGTH_SHORT).show());
     }
 }
